8fff8e327bd511bcd740491811e285f946f08032
Jeremy Dezalos <jeremy.dezalos@epfl.ch>
2024-07-31 16:00:55 +0200

    initial commit

---
+++ BugdetProgramBE/src/main/java/com/budget/BugdetProgram/BugdetProgramApplication.java
@@ -0,0 +1,13 @@
+package com.budget.BugdetProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugdetProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugdetProgramApplication.class, args);
+	}
+
+}
---
+++ BugdetProgramBE/src/test/java/com/budget/BugdetProgram/BugdetProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.BugdetProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugdetProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
